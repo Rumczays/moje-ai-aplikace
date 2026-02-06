11f17bc8556c9451cb7d6fec3bdb8016fdd5b094
Rumczays <rumczays@gmail.com>
2026-02-06 09:12:47 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone', // Toto pomáhá Vercelu lépe pochopit strukturu
+};
 
 export default nextConfig;
